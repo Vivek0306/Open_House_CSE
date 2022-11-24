cb1be46f009d36acce8feb7c15fdc28da5de0a53
Vivek Nair <vivekmanju53@gmail.com>
2022-11-24 14:51:27 +0530

    Scroll fix

--- 3_JS/scripts.js
+++ 3_JS/scripts.js
@@ -51,7 +51,8 @@ function Scroll(){
     window.scroll(0, x);
     x = x + 2;
     if (x > y){
-        setTimeout(function (){location.reload()}, 100);
+        location.reload()
+        // setTimeout(function (){location.reload()}, 100);
     }
     requestAnimationFrame(Scroll)
 }
