e2ba8eeaf88733bed43bfbe3e528a0d846a4e54c
Vivek Nair <vivekmanju53@gmail.com>
2022-11-25 10:11:21 +0530

    Added counter

--- 3_JS/scripts.js
+++ 3_JS/scripts.js
@@ -1,3 +1,9 @@
+import { CountUp } from './js/dist/countUp.min.js';
+
+window.onload = function() {
+    const countUp = new CountUp('target', 3896, { enableScrollSpy: true });
+}
+
 window.addEventListener('DOMContentLoaded', event => {
 
     // Navbar shrink function
@@ -57,3 +63,4 @@ function Scroll(){
     requestAnimationFrame(Scroll)
 }
 Scroll()
+
